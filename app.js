6e13bd279e1a63c994da2949d9e84e394bdd5207
Jesus Flores Roman <jesus.flores14@unmsm.edu.pe>
2023-06-17 01:01:22 -0500

    Webserver Listo

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const hbs = require('hbs');
+require('dotenv').config();
+const app = express()
+const port = process.env.PORT;
+app.set('view engine', 'hbs'); // para usar el handlebars para express
+hbs.registerPartials(__dirname + '/views/partials'); //para usar parciales
+//para servir contenido estatico
+app.use(express.static('public')); //definicion de midelware express. si public esta en otro sitio especificar path
+
+
+// app.get('/', (req, res)=> {
+//     res.send('Hello World')
+// })
+
+app.get('/', (req, res)=> {
+    res.render('home',{ // estos son los argumentos
+        nombre: 'Armando David',
+        titulo: 'Curso de Node'
+    });
+})
+
+app.get('/hola-mundo', (req, res)=> {
+    res.send('Hola Mundo en su respectiva ruta')
+})
+
+app.get('/elements', (req, res) => {
+    // res.sendFile(__dirname + '/public/elements.html')
+    res.render('elements', {
+        nombre: 'Armando David',
+        titulo: 'Curso de Node'
+    })
+});
+
+app.get('/generic', (req, res) => {
+    // res.sendFile(__dirname + '/public/generic.html')
+    res.render('generic', {
+        nombre: 'Armando David',
+        titulo: 'Curso de Node'
+    })
+});
+
+app.get('*', (req, res)=> {
+    // res.send('404 | Page not found')
+    res.sendFile(__dirname+'/public/404.html');
+})
+
+app.listen(port, () => {
+    console.log(`Example app listenining al http://localhost:${port}`)
+})
